9ccd9a0a638fc2be334476ddfe6048fe139070d2
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-06-14 18:42:16 +0900

    work on 3

---
+++ 3 Longest Substring Without Repeating.js
@@ -0,0 +1,31 @@
+var lengthOfLongestSubstring = function(s) {
+    // Have a start string position
+    // Record position of start and add a sliding position at the next spot
+    // Check each next position as you slide down
+    // When you get a dupe, increment the start position by one and compare against the current max
+    // Adjust max if necessary
+
+    let startPos = 0;
+    let subStr;
+    let subChar; 
+    let maxLength = 0;
+
+    for (const char in s) {
+        subStr = char;
+
+        for (let i = startPos+1; i < s.length; i++) {
+            subChar = s[i];
+
+            if (subStr.includes(subChar)) {
+                break;
+            }
+
+            subStr = subStr+subChar;
+        }
+
+        if (subStr.length > maxLength) maxLength = subStr.length;
+    }
+
+    return maxLength;
+
+};
