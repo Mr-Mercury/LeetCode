4376d90fad83c2964da9eaddef8c1b4972a88d5f
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-08-07 18:13:02 +0900

    Recommit

--- 110 Balanced Binary Tree.py
+++ 110 Balanced Binary Tree.py
@@ -14,4 +14,6 @@ class Solution:
             balance = left[0] and right[0] and abs(left[1] - right[1]) <= 1
 
             return [balance, 1 + max(left[1], right[1])]
+        
+        return dfs(root)[0]
 
