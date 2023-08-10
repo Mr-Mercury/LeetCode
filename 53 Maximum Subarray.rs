78d67b6b376ec32960ae539e579ff28a69ea7a52
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-08-10 11:24:53 +0900

    Max subarray rust solution

--- 53 Maximum Subarray.rs
+++ 53 Maximum Subarray.rs
@@ -1,21 +1,20 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        sumMax = float('-inf')
-        sumCur = 0
-
-        for num in nums:
-            sumCur += num
-            
-            if sumCur > sumMax:
-                sumMax = sumCur
-            
-            if sumCur < 0:
-                sumCur = 0
-        
-        return sumMax
-
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        let mut sumMax = 
+        
+        let mut sumCur: i32 = 0;
+        let mut sumMax: i32 = i32::MIN;
+
+        for i in 0..nums.len() {
+            sumCur += nums[i];
+            
+            if sumCur > sumMax {
+               sumMax = sumCur;
+            }
+
+            if sumCur < 0 {
+                sumCur = 0;
+            }
+        }
+        sumMax
     }
 }
